b5dea00f5344b981a4b7bad8fae923f9177252bc
Bruno Leon Pucarelli <brunopuca1@gmail.com>
2020-12-05 12:46:08 -0300

    small improvements pt 22

--- js/scripts.js
+++ js/scripts.js
@@ -59,12 +59,35 @@
     const strText = text.textContent;
     const splitText = strText.split("");
     text.textContent = "";
+    
+    var style = "<div style='display:inline-block';>";
+    var style_end = "</div>";
+    var content = "";
+    let flag = null;
+    
 
     for (let i = 0; i < splitText.length; i++)
     {
-        let char = (splitText[i] === " ") ? "&nbsp;" : splitText[i];
-        text.innerHTML += "<span>" + char + "</span>";
+        let char = 0;
+        
+        if (splitText[i] === " ") {
+            char = "&nbsp;"
+        } else {
+            char = splitText[i];
+        }
+        if ((flag) || (i==0)) {
+            content += style + "<span>" + char + "</span>";
+            flag = false
+        }
+        else if (char == "&nbsp;") {
+            content += "<span>" + char + "</span>" + style_end;
+            flag = true;
+        }
+        else {
+            content += "<span>" + char + "</span>";
+        }
     }
+    text.innerHTML = content;
 
     let char = 0;
     let timer = setInterval(onTick, 80);
