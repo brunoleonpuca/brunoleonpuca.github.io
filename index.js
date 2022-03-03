f9f4c4106b4573b7bb1f0de5dee90d263702ff8a
Leon <brunopuca1@gmail.com>
2022-03-03 12:11:25 +0100

    Initial commit of real responsive portfolio

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
 $(document).ready(function () {
-  $("#myModal").modal('show');
-});
-
-$("#myModal").on("show", function () {
-  $("body").addClass("modal-open");
-}).on("hidden", function () {
-  $("body").removeClass("modal-open")
-});
-
-(function ($) {
-  "use strict"; // Start of use strict
-
-
-  // Closes responsive menu when a scroll trigger link is clicked
-  $(".js-scroll-trigger").click(function () {
-    $(".navbar-collapse").collapse("hide");
-  });
-
-  $(document).on("click", function () {
-    $(".navbar-collapse").collapse("hide");
-  });
-
-  $('div[class="project-hover"]').click(function (e) {
-    // <--- don't miss this e
-    if ($(this).css("opacity") == 0) e.preventDefault();
-
-  });
+  "use strict"
 
   const text = document.querySelector("#name");
-  console.log(text);
   const strText = text.textContent;
   const splitText = strText.split("");
   text.textContent = "";
@@ -75,11 +48,4 @@ $("#myModal").on("show", function () {
     clearInterval(timer);
     timer = null;
   }
-
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-    $('[data-toggle="tooltip"]').on("shown.bs.tooltip", function () {
-      $(".tooltip").addClass("animated swing");
-    });
-  });
-})(jQuery);
+});
---
+++ static/js/scroll.js
@@ -0,0 +1,179 @@
+"use strict"
+
+const slides = document.querySelectorAll("section");
+const container = document.querySelector("#panelWrap");
+let dur = 0.5;
+let offsets = [];
+let oldSlide = 0;
+let activeSlide = 0;
+let dots = document.querySelector(".dots");
+let navDots = [];
+let iw = window.innerWidth;
+
+document.querySelector("#leftArrow").addEventListener("click", slideAnim);
+document.querySelector("#rightArrow").addEventListener("click", slideAnim);
+
+// create the nav dots
+for (let i = 0; i < slides.length; i++) {
+    let newDot = document.createElement("div");
+    newDot.className = "dot";
+    newDot.index = i;
+    navDots.push(newDot);
+    newDot.addEventListener("click", slideAnim);
+    dots.appendChild(newDot);
+}
+
+// get elements positioned
+gsap.set(".dots, .titleWrap", {
+    xPercent: -50
+});
+gsap.set(".arrow", {
+    yPercent: -50
+});
+gsap.set(".title", {
+    y: 30
+});
+
+// lower screen animation with nav dots and rotating titles
+const dotAnim = gsap.timeline({
+    paused: true
+});
+dotAnim.to(
+    ".dot", {
+        stagger: {
+            each: 1,
+            yoyo: true,
+            repeat: 1
+        },
+        scale: 2.1,
+        rotation: 0.1,
+        ease: "none"
+    },
+    0.5
+);
+dotAnim.to(
+    ".title",
+    slides.length + 1, {
+        y: -(slides.length * 30),
+        rotation: 0.01,
+        ease: "none"
+    },
+    0
+);
+dotAnim.time(1);
+
+// make the whole thing draggable
+let dragMe = Draggable.create(container, {
+    type: "x",
+    edgeResistance: 1,
+    snap: offsets,
+    inertia: true,
+    bounds: "#masterWrap",
+    onDrag: tweenDot,
+    onThrowUpdate: tweenDot,
+    onDragEnd: slideAnim,
+    allowNativeTouchScrolling: false,
+    zIndexBoost: false
+});
+
+dragMe[0].id = "dragger";
+sizeIt();
+
+// main action check which of the 4 types of interaction called the function
+function slideAnim(e) {
+    oldSlide = activeSlide;
+    // dragging the panels
+    if (this.id === "dragger") {
+        activeSlide = offsets.indexOf(this.endX);
+    } else {
+        if (gsap.isTweening(container)) {
+            return;
+        }
+        // arrow clicks
+        if (this.id === "leftArrow" || this.id === "rightArrow") {
+            activeSlide =
+                this.id === "rightArrow" ? (activeSlide += 1) : (activeSlide -= 1);
+            // click on a dot
+        } else if (this.className === "dot") {
+            activeSlide = this.index;
+            // scrollwheel
+        } else {
+            activeSlide = e.deltaY > 0 ? (activeSlide += 1) : (activeSlide -= 1);
+        }
+    }
+    // make sure we're not past the end or beginning slide
+    activeSlide = activeSlide < 0 ? 0 : activeSlide;
+    activeSlide = activeSlide > slides.length - 1 ? slides.length - 1 : activeSlide;
+    if (oldSlide === activeSlide) {
+        return;
+    }
+    // if we're dragging we don't animate the container
+    if (this.id != "dragger") {
+        gsap.to(container, dur, {
+            x: offsets[activeSlide],
+            onUpdate: tweenDot
+        });
+    }
+}
+
+// update the draggable element snap points
+function sizeIt() {
+    offsets = [];
+    iw = window.innerWidth;
+    gsap.set("#panelWrap", {
+        width: slides.length * iw
+    });
+    gsap.set(slides, {
+        width: iw
+    });
+    for (let i = 0; i < slides.length; i++) {
+        offsets.push(-slides[i].offsetLeft);
+    }
+    gsap.set(container, {
+        x: offsets[activeSlide]
+    });
+    dragMe[0].vars.snap = offsets;
+}
+
+gsap.set(".hideMe", {
+    opacity: 1
+});
+window.addEventListener("wheel", slideAnim);
+window.addEventListener("resize", sizeIt);
+
+// update dot animation when dragger moves
+function tweenDot() {
+    gsap.set(dotAnim, {
+        time: Math.abs(gsap.getProperty(container, "x") / iw) + 1
+    });
+}
+
+gsap.to("#floating-button", {
+    autoAlpha: 1,
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#experience",
+        start: "bottom bottom",
+        scrub: true,
+    },
+});
+
+gsap.to("#floating-button", {
+    autoAlpha: 1,
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#experience",
+        start: "bottom bottom",
+        scrub: true,
+    },
+});
+
+gsap.to(".social-icons-floating", {
+    autoAlpha: 1,
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#experience",
+        start: "bottom bottom",
+        scrub: true,
+    },
+});
