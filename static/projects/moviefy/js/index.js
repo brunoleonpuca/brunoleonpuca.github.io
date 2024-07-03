862c98bd221566e1f65764e63342730229dc9c4b
unknown <brunopuca1@gmail.com>
2024-07-03 20:21:40 +0200

    moviefy project added into projects: wip

---
+++ static/projects/moviefy/js/index.js
@@ -0,0 +1,11 @@
+document.querySelector('.logo').addEventListener('click', function(e) {
+    window.location.href = 'index.html';
+});
+
+document.getElementById('createUserBtn').addEventListener('click', function(e) {
+    window.location.href = 'create-user.html';
+});
+
+document.getElementById('loginBtn').addEventListener('click', function(e) {
+    window.location.href = 'login.html';
+});
